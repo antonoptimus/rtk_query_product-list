87bc4e6a531151e165be64443b146ebf0676eb13
Anton Eliseev <anton_saintp@mail.ru>
2024-02-17 20:32:55 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  useAddProductMutation,
+  useDeleteProductMutation,
+  useGetGoodsQuery,
+} from "./redux/goodsApi";
 
 function App() {
+  const [count, setCount] = useState("");
+  const [newProduct, setNewProduct] = useState("");
+
+  const { data = [], isError, isLoading } = useGetGoodsQuery(count);
+  const [addProduct] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct("");
+    }
+  };
+
+  const handleDeleteProduct = async (id) => {
+    await deleteProduct(id).unwrap();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isError && <h1>Something went wrong...</h1>}
+      {isLoading && <h1>Loading...</h1>}
+
+      <form>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={(e) => setNewProduct(e.target.value)}
+        />
+        <button onClick={handleAddProduct}>add</button>
+      </form>
+
+      <select value={count} onChange={(e) => setCount(e.target.value)}>
+        <option value="">all</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+
+      <ul>
+        {data.map((item) => (
+          <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
